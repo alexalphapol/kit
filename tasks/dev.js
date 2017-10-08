d6a62fea057fc01d968aca6151a9ddd14b1a4785
Alex Pol <alexalphapol@gmail.com>
2017-10-08 19:15:16 +0300

    initial commit

--- tasks/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = function (grunt) {
-    grunt.registerTask('default', ['browserify:dev', 'concat:dev_js', 'concat:dev_css', 'autoprefixer:dev']);
-    grunt.registerTask('dev', ['watch']);
-};
